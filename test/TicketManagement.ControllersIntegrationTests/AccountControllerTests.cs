facd5b82d0d48e47dc91c177130e9f3939704266
Ilya-Rebikov <ilya.rebikov.2000@mail.ru>
2022-02-09 19:09:08 +0300

    Done with integrations tests for controllers

---
+++ test/TicketManagement.ControllersIntegrationTests/AccountControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using TicketManagement.Web;
+using Xunit;
+
+namespace TicketManagement.ControllersIntegrationTests
+{
+    public class AccountControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public AccountControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/Account/Register")]
+        [InlineData("/Account/Login")]
+        [InlineData("/Account/Edit")]
+        [InlineData("/Account/AddBalance")]
+        [InlineData("/Account/Index")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
---
+++ test/TicketManagement.ControllersIntegrationTests/TicketsControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using TicketManagement.Web;
+using Xunit;
+
+namespace TicketManagement.ControllersIntegrationTests
+{
+    public class TicketsControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public TicketsControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/Tickets/Index")]
+        [InlineData("/Tickets/Create")]
+        [InlineData("/Tickets/Edit")]
+        [InlineData("/Tickets/Delete")]
+        [InlineData("/Tickets/Details")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
