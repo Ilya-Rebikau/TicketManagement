b7ca74441b95008204e6a35987b3cffa1b9ee824
Ilya-Rebikov <ilya.rebikov.2000@mail.ru>
2022-02-24 14:14:14 +0300

    Updated ninject work and nugets

--- src/ThirdPartyEventEditor/ThirdPartyEventEditor/Global.asax.cs
+++ src/ThirdPartyEventEditor/ThirdPartyEventEditor/Global.asax.cs
@@ -1,10 +1,6 @@
-﻿using Ninject;
-using Ninject.Modules;
-using Ninject.Web.Mvc;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using ThirdPartyEventEditor.App_Start;
 
 namespace ThirdPartyEventEditor
 {
@@ -16,9 +12,6 @@ namespace ThirdPartyEventEditor
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            NinjectModule services = new ServicesConfig();
-            var kernel = new StandardKernel(services);
-            DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
         }
     }
 }
