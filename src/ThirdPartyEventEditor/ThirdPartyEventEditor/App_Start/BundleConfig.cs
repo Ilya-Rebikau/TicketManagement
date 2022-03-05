49f5da85cd0641a8df050e098aa11f77aa016c52
Ilya-Rebikov <ilya.rebikov.2000@mail.ru>
2022-03-05 17:33:35 +0300

    using libman now.

--- src/ThirdPartyEventEditor/ThirdPartyEventEditor/App_Start/BundleConfig.cs
+++ src/ThirdPartyEventEditor/ThirdPartyEventEditor/App_Start/BundleConfig.cs
@@ -8,22 +8,22 @@ namespace ThirdPartyEventEditor
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/wwwroot/lib/jquery/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/wwwroot/lib/jquery-validate/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/wwwroot/lib/modernizr/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/js/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/bundles/bootstrap").Include(
+                      "~/wwwroot/lib/Bootstrap/css/bootstrap.css",
+                      "~/wwwroot/css/Site.css"));
         }
     }
 }
